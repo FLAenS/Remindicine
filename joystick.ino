27796388c8fecc92a05761ea2302b4a231743218
Cera <75736027+acrenw@users.noreply.github.com>
2023-11-16 05:50:34 -0800

    formatted numbers nicely

--- joystick.ino
+++ joystick.ino
@@ -6,17 +6,17 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 #define VRY_PIN  A1 // Arduino pin connected to VRY pin
 #define SW_PIN   2  // Arduino pin connected to SW  pin
 
-#define COMMAND_NO     0x00
-#define COMMAND_LEFT   0x01
-#define COMMAND_RIGHT  0x02
-#define COMMAND_UP     0x04
-#define COMMAND_DOWN   0x08
+// #define COMMAND_NO     0x00
+// #define COMMAND_LEFT   0x01
+// #define COMMAND_RIGHT  0x02
+// #define COMMAND_UP     0x04
+// #define COMMAND_DOWN   0x08
 
-bool switchstate = false;
+// bool switchstate = false;
 int xValue = 0; // To store value of the X axis
 int yValue = 0; // To store value of the Y axis
 int bValue = 0; // To store value of the button
-int command = COMMAND_NO;
+// int command = COMMAND_NO;
 int xdigit = 0;
 int ydigit = 0;
 
@@ -30,12 +30,10 @@ void setup()
 
 void loop()
 {
-  switchstate=false;
+  // switchstate=false;
   xValue = analogRead(VRX_PIN);
   yValue = analogRead(VRY_PIN);
 
-  lcd.clear();
-  lcd.setCursor(0, 0); 
   if (xValue < 250) {
     if ((xdigit-1)>=0) {
       xdigit -= 1;
@@ -52,12 +50,20 @@ void loop()
       xdigit = 0;
     }
   }
-  lcd.print(xdigit);
-
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (xdigit<10) {
+    lcd.print("0");
+    lcd.setCursor(1, 0)
+    lcd.print(xdigit);
+  }
+  else {
+    lcd.print(xdigit);
+  }
+  
   lcd.setCursor(2, 0);
   lcd.print(":");
 
-  lcd.setCursor(3, 0); 
   if (yValue > 750) {
     if ((ydigit-1)>=0) {
       ydigit -= 1;
@@ -74,11 +80,21 @@ void loop()
       ydigit = 0;
     }
   }
-  lcd.print(ydigit);
-
-  if(digitalread(2)){
-    switchstate=true;
+  
+  lcd.setCursor(3, 0); 
+  if (ydigit<10) {
+    lcd.print("0");
+    lcd.setCursor(4, 0);
+    lcd.print(ydigit);
   }
+  else {
+    lcd.print(ydigit);
+  }
+  
+
+  // if(digitalRead(2)){
+  //   switchstate=true;
+  // }
   delay(1000/4); 
 }
 
