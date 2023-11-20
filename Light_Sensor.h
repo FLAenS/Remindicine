b306846d15087b8373eab7beb7d9e1840a97bb1d
FLAenS <145291657+FLAenS@users.noreply.github.com>
2023-11-20 01:34:02 -0500

    update sensor detection

--- Light_Sensor.h
+++ Light_Sensor.h
@@ -8,17 +8,30 @@
 #ifndef INC_LIGHT_SENSOR_H_
 #define INC_LIGHT_SENSOR_H_
 
+int OFF = 0;
+
 void Light_Sensor()
 {
 	int lux = rawValues[3];
-	if (hour == 1)
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 1);
-	GPIO_PinState pinState = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9);
-	if (pinState)
-	{
-		if (lux > 3500) HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
-	}
 
+	if (hour == 0 && minute == 0 && second == 0)
+	{
+		if (OFF == 0)
+		{
+			// Blink the LED for one second
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 1); // Turn ON the LED
+			HAL_Delay(1000);// Delay for one second
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0); // Turn OFF the LED
+			HAL_Delay(1000); // Delay for one second
+		}
+	}
+	else HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
+
+	if (lux > 3500)
+	{
+		OFF = 1;
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
+	}
 
 }
 
